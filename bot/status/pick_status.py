34f0cc627cca8021347de96c32428f729e3a192d
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-05-23 00:29:56 -0400

    add statuses

--- bot/status/pick_status.py
+++ bot/status/pick_status.py
@@ -4,12 +4,14 @@ from random import choice, shuffle
 # ---------------------------------------------------------------------------- #
 def pick_status():
     status_options = [
+        "https://www.facebook.com/jharriswebdev #freelance #webdev #100DaysOfCode #techjobs",
          "If you were a triangle you'd be acute one.",
         "eBay is so useless. I tried to look up lighters and all they had was 13,749 matches.",
         "(┛ಠ_ಠ)┛彡┻━┻",
         "Check out my creator's portfolio here: https://jharriswebdev.herokuapp.com/ #freelance #webdeveloper #coding #100DaysOfCode",
         "My creator is kind of funny, too. Check him out: @jheeeeezy #bot",
         "@jheeeeezy is the one that created me! Check him out!",
+        "Follow me here too:  https://www.facebook.com/jharriswebdev !!! #freelance #webdev #100DaysOfCode #techjobs",
         "Beep boop boop bmmmmmmmmm *~*laser sounds*~* beep",
         "Best bot in the biz, baby! #bot #webdev #coding #python",
         "*- Does robot dance to future music -*",
@@ -17,6 +19,7 @@ def pick_status():
         "これは英語ではありません！",
         "@jheeeeezy can create a bot for you, too! #bot #freelance #hitmeup #work",
         "https://jharriswebdev.herokuapp.com/ #freelance #webdev",
+        "Go like the Facebook Page: https://www.facebook.com/jharriswebdev !!! #freelance #webdev #100DaysOfCode #techjobs"
     ]
     shuffle(status_options)
     return choice(status_options)
